2f8f651fcf2e9514aea060badc87f7bda34812c8
Natalia Regina Lima Silva <natalia_regina10@outlook.com>
2023-06-30 23:59:01 -0300

    Incluindo insercao de texto

--- frontend/app/src/main/java/com/censobrasilapp/FaceTela.kt
+++ frontend/app/src/main/java/com/censobrasilapp/FaceTela.kt
@@ -1,19 +1,28 @@
 package com.censobrasilapp
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.censobrasilapp.databinding.FaceTelaBinding
-import com.google.android.material.progressindicator.LinearProgressIndicator
+import com.google.android.material.textfield.TextInputLayout
+
 
 class FaceTela : Fragment() {
 
     private var _binding: FaceTelaBinding? = null
     private val binding get() = _binding!!
-    lateinit var linear: LinearProgressIndicator
+    private lateinit var activityContext: Context
+    //banco de dados
+    private val items_logradouro = listOf("Rua Hipódromo", "Rua Inácio de Araújo", "Rua 21 de Abril", "Avenida Celso Garcia")
+    private val items_bairro = listOf("Brás", "Ipiranga") //banco de dados
+
+    private var text: TextInputLayout? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,16 +32,44 @@ class FaceTela : Fragment() {
         _binding = FaceTelaBinding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onAttach(context: Context) {
+        activityContext = context
+        super.onAttach(context)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.proximoFaceBtn.setOnClickListener {
+            val textInputLayout: TextInputLayout = view.findViewById(R.id.quadra_input)
+            val text: String = textInputLayout.editText?.text.toString()
+
+            Log.i("alo", text)
             findNavController().navigate(R.id.action_FaceTela_to_UnidadeTela)
         }
-        //binding.indicator.setProgressCompat(50, true)
-        //linear.setProgressCompat(50, true)
+
+        binding.logradouroAuto.apply {
+            setAdapter(
+                ArrayAdapter(
+                    activityContext,
+                    R.layout.dropdown_id,
+                    items_logradouro
+                )
+            )
+        }
+
+        binding.bairroAuto.apply {
+            setAdapter(
+                ArrayAdapter(
+                    activityContext,
+                    R.layout.dropdown_id,
+                    items_bairro
+                )
+            )
+        }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
