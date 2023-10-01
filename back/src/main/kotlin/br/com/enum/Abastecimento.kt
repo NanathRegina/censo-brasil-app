3623c09163d0715fa5bec674da49350eed5d1532
Natalia Regina Lima Silva <natalia_regina10@outlook.com>
2023-09-30 23:11:49 -0300

    ajustes para o front

--- back/src/main/kotlin/br/com/enum/Abastecimento.kt
+++ back/src/main/kotlin/br/com/enum/Abastecimento.kt
@@ -3,8 +3,8 @@ package br.com.enum
 enum class Abastecimento(
     val tipo: String
 ) {
-    REDE_GERAL("Rede geral"),
+    REDE_GERAL("Rede geral de distribuição"),
     POCO("Poço"),
-    CARRO_PIPA("Carro pipa"),
-    CHUVA("Chuva")
+    CARRO_PIPA("Carro-pipa"),
+    CHUVA("Água da chuva armazenada")
 }
