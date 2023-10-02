e18e88a3537a9446beb1346203b2ad8810efabe6
Matheus Lima <matheusldasilva20088@gmail.com>
2023-10-01 21:34:18 -0300

    feat: add client for retrofit

--- frontend/app/src/main/java/com/censobrasilapp/utils/NetworkUtils.kt
+++ frontend/app/src/main/java/com/censobrasilapp/utils/NetworkUtils.kt
@@ -1,8 +1,10 @@
 package com.censobrasilapp.utils
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+import java.util.concurrent.TimeUnit
 
 
 private const val BASE_URL = "https://censo-backend-f9dc3fb9183b.herokuapp.com/"
@@ -12,8 +14,16 @@ class NetworkUtils {
     companion object {
 
         fun getRetrofitInstance() : Retrofit {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
+
             return Retrofit.Builder()
-                .baseUrl("$BASE_URL")
+                .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
